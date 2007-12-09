1b3411fd458ada2a2db6c9431d6910003af1d829
David M. Syzdek <syzdek@users.sourceforge.net>
2007-12-09 15:55:12 +0000

    added error checking

--- gnu/gendist.sh
+++ gnu/gendist.sh
@@ -15,23 +15,27 @@ PREFIX="/tmp/roman-$RELEASE"
 rm -Rf ${PREFIX}
 
 ${SRCDIR}/configure	--prefix=${PREFIX} --disable-shared \
-			--host=i386-mingw32 --enable-strictwarnings
-make
-make dist
-make dist-bzip2
-make dist-zip
-make txtdocs
-make windist
+			--host=i386-mingw32 --enable-strictwarnings || exit 1
+make || exit 1
+make dist || exit 1
+make dist-bzip2 || exit 1
+make dist-zip || exit 1
+make txtdocs || exit 1
+make windist || exit 1
 
 
-mkdir -p   ~/Desktop/release-${1}
-mv *.tar.* ~/Desktop/release-${1}/
-cp *.zip   ~/Desktop/release-${1}/
+mkdir -p   ~/Desktop/release-${1} || exit 1
+mv *.tar.* ~/Desktop/release-${1}/ || exit 1
+cp *.zip   ~/Desktop/release-${1}/ || exit 1
 
-rm -fR /tmp/roman-$RELEASE/share
-cd /tmp
-zip -r roman-$RELEASE.zip roman-$RELEASE
-mv roman-$RELEASE.zip ~/Desktop/release-${1}/
+rm -fR /tmp/roman-$RELEASE/share || exit 1
+cd /tmp || exit 1
+zip -r roman-$RELEASE.zip roman-$RELEASE || exit 1
+mv roman-$RELEASE.zip ~/Desktop/release-${1}/ || exit 1
 
-date > ~/Desktop/release-${1}/timestamp.txt
+date > ~/Desktop/release-${1}/timestamp.txt || exit 1
+
+echo " "
+echo " Finished with no errors"
+echo " "
 

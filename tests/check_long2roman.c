fec26f8983e34bf2ea8da910ebc54c5aac1589f3
David M. Syzdek <syzdek@users.sourceforge.net>
2007-12-12 05:21:48 +0000

    added comments and copyright to tests

--- tests/check_long2roman.c
+++ tests/check_long2roman.c
@@ -1,3 +1,26 @@
+/*
+ *  $Id$
+ *
+ *  Roman Numeral Conversion API
+ *  Copyright (c) 2007 David M. Syzdek <roman-project@syzdek.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *  tests/check_roman2long.c - tests roman2long
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <roman.h>
@@ -8,14 +31,14 @@ int main(int argc, char * argv[])
 {
    int    i;
    int    v;
-   char   buff[512];
+   char   buff[ROMAN_BUFF_LEN];
 
    v = 0;
    if (argc > 1)
       if (!(strcmp(argv[1], "-v")))
          v = 1;
 
-   for(i = 0; i < 5000; i++)
+   for(i = ROMAN_NUMERAL_MIN; i < ROMAN_NUMERAL_MAX; i++)
    {
       if (!(long2roman_r(i, buff, 512)))
       {
@@ -41,3 +64,5 @@ int main(int argc, char * argv[])
 
    return(0);
 }
+
+/* end of source */

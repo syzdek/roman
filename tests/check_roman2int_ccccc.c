6779d219f9c6f37f919d02574eb2f58360c46ce5
David M. Syzdek <syzdek@users.sourceforge.net>
2008-01-21 14:49:21 +0000

    added tests for new pattern checks

---
+++ tests/check_roman2int_ccccc.c
@@ -0,0 +1,39 @@
+/*
+ *  $Id$
+ *
+ *  Roman Numeral Conversion API
+ *  Copyright (c) 2007 David M. Syzdek <roman-project@syzdek.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *  tests/check_int2roman.c - tests int2roman
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <roman.h>
+
+int main(void);
+
+int main(void)
+{
+   if (roman2int("CCCCC") != -1)
+      return(1);
+   if (roman2int("DCCCCC") != -1)
+      return(1);
+   return(0);
+}
+
+/* end of source */
